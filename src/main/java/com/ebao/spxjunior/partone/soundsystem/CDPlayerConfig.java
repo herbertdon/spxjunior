ca1a6f047c46c80bf6f7092eca6db7cd685d811e
dondavid <dondavid@192.168.0.102>
2017-06-15 09:32:17 +0800

    traditional xml config enabled

--- src/main/java/com/ebao/spxjunior/partone/soundsystem/CDPlayerConfig.java
+++ src/main/java/com/ebao/spxjunior/partone/soundsystem/CDPlayerConfig.java
@@ -1,9 +1,6 @@
 package com.ebao.spxjunior.partone.soundsystem;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan
+//@Configuration
+//@ComponentScan
 public class CDPlayerConfig {
 }
