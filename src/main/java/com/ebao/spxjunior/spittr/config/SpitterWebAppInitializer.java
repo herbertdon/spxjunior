15a9f56c56cea5ec53c8705c01c7c6e6d306c0c5
herbert.tang <dondavd@163.com>
2017-07-05 10:08:32 +0800

    spring mvc dependency added

---
+++ src/main/java/com/ebao/spxjunior/spittr/config/SpitterWebAppInitializer.java
@@ -0,0 +1,22 @@
+package com.ebao.spxjunior.spittr.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpitterWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return null;
+	}
+	
+}
